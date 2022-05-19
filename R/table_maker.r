82bf208c31593e99b116939c47d94edb44da33d9
Kisters <tom.kisters@wur.nl>
2022-05-19 10:37:29 +0200

    Update table_maker.r

--- R/table_maker.r
+++ R/table_maker.r
@@ -233,6 +233,7 @@ table_maker <- function(table_in, strata_in = NULL) {
     }
 
     frequency_table <- merge(frequency_table, when_strata_in_is_null, all.x=TRUE, by.x="rn", by.y="ENG_name")
+    
     print("7")
 
     ##########################################################################################
